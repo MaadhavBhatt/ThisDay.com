7b1e6cca53f0f663804781e1469a73e9e8520b83
MaadhavBhatt <81471860+MaadhavBhatt@users.noreply.github.com>
2022-04-18 15:40:13 +0530

    Rename menu_toggle to script and add typewriter

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,4 @@
+// Menu toggle
 const nav = document.querySelector('#nav');
 const menuBtn = document.querySelector('#menuToggle');
 
@@ -5,3 +6,7 @@ menuBtn.addEventListener('click', () => {
   nav.classList.toggle('open');
   nav.classList.toggle('display-none');
 });
+
+// Typewriter
+const typewriter = document.querySelector('.typewriter');
+typewriter.style.setProperty('--typewriter-chars', typewriter.textContent.length);
